4aeaf1f569e068f7176cffa7beea8d57bdf2c0f8
NavinduR2001 <164781229+NavinduR2001@users.noreply.github.com>
2025-09-28 01:23:21 +0530

    Update assets.jsx

--- frontend/src/assets/assets.jsx
+++ frontend/src/assets/assets.jsx
@@ -1,6 +1,12 @@
 import logo from './logo.png';
 import cart from './cart.png';
+import branch from './branch.jpg';
+import chicken from './Welcome-chicken.png';
+import rplate from './r-plate.png';
+import lplate from './l-plate.png';
+import rostedChicken from './rostedchicken.png';
+import pizza from './pizzadish.png';
 
 export {
-    logo,cart,
+    logo,cart,branch,chicken,rplate,lplate,rostedChicken,pizza
 }
