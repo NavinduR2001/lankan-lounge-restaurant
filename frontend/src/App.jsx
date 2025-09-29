37aed604497e428252cc3c3d178cfa4b12a87d42
NavinduR2001 <164781229+NavinduR2001@users.noreply.github.com>
2025-09-29 20:08:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Home from './pages/home/home'
+import ContactUs from './pages/contact-us/ContactUs'
+import AboutUs from './pages/about us/AboutUs'
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
     <Routes>
 
       <Route path='/' element={<Home/>}></Route>
+      <Route path='/contact-us' element={<ContactUs/>}></Route>
+      <Route path='/about-us' element={<AboutUs/>}></Route>
     </Routes>
     </BrowserRouter>
   )
