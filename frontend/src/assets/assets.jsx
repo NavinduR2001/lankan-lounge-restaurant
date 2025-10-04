977e8be4695b5eab9f35f6be8d16d018db382188
NavinduR2001 <164781229+NavinduR2001@users.noreply.github.com>
2025-10-04 23:45:39 +0530

    update base files

--- frontend/src/assets/assets.jsx
+++ frontend/src/assets/assets.jsx
@@ -6,7 +6,20 @@ import rplate from './r-plate.png';
 import lplate from './l-plate.png';
 import rostedChicken from './rostedchicken.png';
 import pizza from './pizzadish.png';
+import burger from './burger.png';
+import biriyani from './biriyani.png';
+import one from './1.jpg';
+import two from './2.jpeg';
+import three from './3.jpg';
+import four from './4.jpg';
+import five from './5.jpg';
+import six from './6.jpg';
+import seven from './7.jpeg';
+import eight from './8 (2).jpg';
+import nine from './9.jpg';
+import ten from './10.png';
 
 export {
-    logo,cart,branch,chicken,rplate,lplate,rostedChicken,pizza
+    logo,cart,branch,chicken,rplate,lplate,rostedChicken,pizza,burger,biriyani,one,two,three,four,five,six,seven,eight,nine,ten
+
 }
