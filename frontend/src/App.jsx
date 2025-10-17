62c52259b2b4bc3bdf473972f2d61070df1ed635
NavinduR2001 <164781229+NavinduR2001@users.noreply.github.com>
2025-10-17 23:59:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,39 @@
-import React from 'react'
-import Home from './pages/home/home'
-import ContactUs from './pages/contact-us/ContactUs'
-import AboutUs from './pages/about us/AboutUs'
-import Menu from './pages/menu/Menu'
-import Register from './pages/register/Register'
-import Login from './pages/login/Login'
-import Cart from './pages/cart/Cart'
-import Admin from './pages/admin-dashboard/Admin'
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './components/CartContext';
+
+// Import components
+import Home from './pages/home/Home';
+import ContactUs from './pages/contact-us/ContactUs';
+import AboutUs from './pages/about us/AboutUs';
+import Menu from './pages/menu/Menu';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Cart from './pages/cart/Cart';
+import Admin from './pages/admin-dashboard/Admin';
+import AdminSettings from './pages/admin-setting/AdminSettings';
+
+import Checkout from './pages/checkout/Checkout';
+
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/contact-us' element={<ContactUs/>}></Route>
-      <Route path='/about-us' element={<AboutUs/>}></Route>
-      <Route path='/menu' element={<Menu/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/register' element={<Register/>}></Route>
-      <Route path='/cart' element={<Cart/>}></Route>
-      <Route path='/admin-dashboard/*' element={<Admin/>}></Route>
-    </Routes>
-    </BrowserRouter>
-  )
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/contact-us' element={<ContactUs/>} />
+          <Route path='/about-us' element={<AboutUs/>} />
+          <Route path='/menu' element={<Menu/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/admin-dashboard' element={<Admin/>} />
+          <Route path='/admin-settings' element={<AdminSettings/>} />
+          <Route path='/checkout' element={<Checkout/>} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
